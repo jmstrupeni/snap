768e4ed66ac127120cc0202b113bcb637c366a58
jmstrupeni <37669834+jmstrupeni@users.noreply.github.com>
2021-04-17 00:28:04 -0300

    Update snap.py

--- snap.py
+++ snap.py
@@ -2,7 +2,7 @@ import pyautogui, os, time, pyperclip, argparse
 
 parser = argparse.ArgumentParser(
     description = 'Script to take a snapshot, ask for a file name, save it and copy destination path to clipboard.')
-parser.add_argument('-e', help='When given, open file explorer in file destination path.', action='store_true')
+parser.add_argument('-e', '--explorer', help='When given, open file explorer in file destination path.', action='store_true')
 args = parser.parse_args()
 
 p = pyautogui
@@ -13,5 +13,5 @@ filename = p.prompt(text='Enter file name.',title='snap.py')
 screenshot.save(os.path.join(cwd,filename)+'.jpg')
 pyperclip.copy(cwd)
 
-if args.e:
+if args.explorer:
     os.startfile(cwd)
